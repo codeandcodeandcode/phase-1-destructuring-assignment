687629288e02a068193fda9620ebc506fd33fed0
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-28 03:35:30 +0300

    Answer to number 5

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ const [redColor, orangeColor, yellowColor, greenColor, blueColor, indigoColor, v
 console.log(greenColor);
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const [r, o, y, g, b, , v]=colors;
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
