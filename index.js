3706fc3b94ee3f485cfcf3f8379cb0feb12d5140
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-28 03:42:31 +0300

    commit - tested

--- index.js
+++ index.js
@@ -71,3 +71,5 @@ console.log(nestedName);
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const {song2, song4} = nestedMuppet.album.theMuppetMovie;
+//commit - tested
+console.log(song2);
