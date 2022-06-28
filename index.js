2421dd62f2371616eb9c2e1def8759476f7d2295
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-28 03:30:27 +0300

    Answer to Question 3

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ const[a1, , a2, a3, a4] = farmAnimals.split(" ");
 console.log(a1, a2, a3, a4);
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [blackAndWhite, ,  black, pink] = farmAnimals.split(" ");
+
 
 // Arrays
 
