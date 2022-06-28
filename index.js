c281688823fe5530d730d21ef8616f0f4cdbbb21
codeandcodeandcode <codepkcode49@gmail.com>
2022-06-28 03:36:17 +0300

    commit - tested

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ console.log(greenColor);
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 const [r, o, y, g, b, , v]=colors;
+//commit - tested
+console.log(r);
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
